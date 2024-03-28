b84579c96488449b4d57300bdbf240996bb8d321
TamaradeMoura <tamara_moura_13@hotmail.com>
2024-03-28 18:22:43 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import { Global } from "./styles";
-import Categories from "./pages/Categories";
+import Categories from "./pages/categories";
 
 
 
@@ -12,7 +12,7 @@ function App() {
     <Global />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/Categories" element={<Categories />} />
+      <Route path="/categories" element={<Categories />} />
     </Routes>
     <Footer />
     </BrowserRouter>
